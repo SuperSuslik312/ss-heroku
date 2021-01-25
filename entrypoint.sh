d36fe741f708be1be533e5411a857a9f98a085a3
SuperSuslik312 <petyapet19@gmail.com>
2021-01-25 21:55:59 +0300

    Removed nameserver 1.1.1.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -75,8 +75,7 @@ cat <<-EOF > /etc/shadowsocks-libev/config.json
     "reuse_port":true,
     "no_delay":true,
     "plugin": "v2ray-plugin",
-    "plugin_opts":"server;path=${V2_Path}",
-    "nameserver": "1.1.1.1"
+    "plugin_opts":"server;path=${V2_Path}"
 }
 EOF
 
